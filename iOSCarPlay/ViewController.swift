35215477a0f3edc8ee613dc8aedac3ae9c9d5b38
Ashish Awasthi <ashish.awasthi2@harman.com>
2020-02-22 19:42:33 +0530

    #01 Add Initial projects

---
+++ iOSCarPlay/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOSCarPlay
+//
+//  Created by Ashish Awasthi on 22/02/20.
+//  Copyright © 2020 Ashish Awasthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Welcome to iOS CarPlay World"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
